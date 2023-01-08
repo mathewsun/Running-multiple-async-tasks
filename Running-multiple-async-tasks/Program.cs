e01b785f684181a20329404beb6a526d61f42696
mmm <mathew.dubrovin@gmail.com>
2023-01-08 06:36:28 +0300

    init

---
+++ Running-multiple-async-tasks/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Running_multiple_async_tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
